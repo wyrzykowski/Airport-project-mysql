2ebff1a3757cb6c3c35e119734e23cd20b654bb7
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-06-05 02:18:18 +0200

    add CORE rules

--- src/server.js
+++ src/server.js
@@ -22,6 +22,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 const db = connectDb();
 
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "*"
+    );
+    if(req.method==="OPTIONS"){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET')
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(infoRouter);
 app.use(employeeRouter);
 app.use(flightRouter);
